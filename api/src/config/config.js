7b0f17abda8718a8323c7dbd4de7128e6b0e2c81
ertugrulsagdic <38252389+ertugrulsagdic@users.noreply.github.com>
2021-08-03 17:00:02 +0300

    config

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -1,16 +1,23 @@
 module.exports = {
   "development": {
-    "username": "asus",
+    "username": "postgres",
     "password": null,
     "database": "mdp_services_development",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
-    "username": "asus",
+    "username": "postgres",
     "password": null,
     "database": "mdp_services_test",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
+  "production": {
+    "username": "postgres",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
 }
