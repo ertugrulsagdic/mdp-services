cebf88da71b4245f56ced2e317104a6cea01a0c2
ertugrulsagdic <38252389+ertugrulsagdic@users.noreply.github.com>
2021-08-03 17:20:20 +0300

    production config

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -13,11 +13,22 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
-  "production": {
-    "username": "postgres",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  }
+  production: {
+    use_env_variable: "DATABASE_URL",
+    database: "d6gml09d079t",
+    username: "idlgdtxgdzdtap",
+    password:
+        "04a567039b08fe902ae82b99c2c8c832de936efdc29b1c65d9bb1784b6d11bcc",
+    host: "ec2-54-196-65-186.compute-1.amazonaws.com",
+    dialect: "postgres",
+    ssl: true,
+    protocol: "postgres",
+    logging: true,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // <<<<<< YOU NEED THIS
+        },
+    },
+},
 }
